1d26b37a67fadbfb53af26288654c2302eb085d9
golnar-m <golnar@soarrobotics.com>
2023-12-06 02:33:42 +0300

    feat(code): add schema

--- src/bin/schema.rs
+++ src/bin/schema.rs
@@ -1,11 +1,9 @@
 use cosmwasm_schema::write_api;
-
-use data_quality_assurance_template::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+pub use assurance::msg::{InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
-        execute: ExecuteMsg,
         query: QueryMsg,
     }
 }
