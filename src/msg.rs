227421e67fd81fdf1ca741d746560df8c47f44d5
golnar-m <golnar@soarrobotics.com>
2023-12-06 02:33:14 +0300

    feat(code): add initiate message

--- src/msg.rs
+++ src/msg.rs
@@ -1,11 +1,23 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
+use crate::types::Data;
 
 #[cw_serde]
-pub struct InstantiateMsg {}
+pub struct InstantiateMsg {
+    pub data: Data,
+}
 
-#[cw_serde]
-pub enum ExecuteMsg {}
 
 #[cw_serde]
 #[derive(QueryResponses)]
-pub enum QueryMsg {}
+pub enum QueryMsg {
+
+    #[returns(ClientByIndexResponse)]
+    GetClientByIndex { data: Data },
+    
+}
+
+// We define a custom struct for each query response
+#[cw_serde]
+pub struct ClientByIndexResponse {
+    pub qualified: bool,
+}
