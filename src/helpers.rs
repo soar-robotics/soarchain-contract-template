8f0fb19864ecca9ed06d75f674565c8bd9d06c6f
golnar-m <golnar@soarrobotics.com>
2023-12-07 10:53:31 +0300

    refactor(code): cleanup

--- src/helpers.rs
+++ src/helpers.rs
@@ -3,15 +3,10 @@ pub mod test {
     use std::marker::PhantomData;
     use crate::execute;
     use crate::error::ContractError;
-    use std::fmt::Debug;
     use cosmwasm_std::testing::{
         MockApi, MockQuerier, MockStorage,
     };
     use cosmwasm_std::OwnedDeps;
-    use schemars::JsonSchema;
-    use serde::Deserialize;
-    use serde::Serialize;
-
 
     pub fn our_mock_dependencies() -> OwnedDeps<MockStorage, MockApi, MockQuerier> {
         OwnedDeps {
@@ -31,28 +26,10 @@ pub mod test {
                 operation: execute::FullOperation::Set,
                 source_chain: "soarchain".to_string(),
             },
-
         ];
         execute::connection_operations(operations)?;
 
         Ok(deps)
-    }
-
-    #[derive(Serialize, Deserialize, Clone, Eq, PartialEq, JsonSchema, Debug)]
-    pub struct Client {
-        pub address: String,
-        pub index: String,
-        pub score: String,
-    }
-
-    impl Client {
-        pub fn new(address: String, index: String, score: String) -> Self {
-            Client {
-                address: address.to_string(),
-                index: index.to_string(),
-                score: score.to_string(),
-            }
-        }
     }
 
 }
