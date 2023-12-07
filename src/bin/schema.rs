ebcb26bfe95ee102c5bd2b255b3adf3afc5cb1a7
golnar-m <golnar@soarrobotics.com>
2023-12-07 13:54:59 +0300

    refactor(structure): cleanup

--- src/bin/schema.rs
+++
@@ -1,9 +0,0 @@
-use cosmwasm_schema::write_api;
-pub use assurance::msg::{InstantiateMsg, QueryMsg};
-
-fn main() {
-    write_api! {
-        instantiate: InstantiateMsg,
-        query: QueryMsg,
-    }
-}
--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod contract;
-mod error;
-pub mod msg;
-pub mod state;
-pub mod helpers;
-pub mod execute;
-pub mod types;
-
-pub use crate::error::ContractError;
